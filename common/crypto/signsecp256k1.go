99ff4669a790c06b0f4b7ffaf1d67fd9e9fc6247
liuyihua <i@liuyihua.com>
2018-10-10 21:44:10 +0800

    remove crypto/secp256k1 to github.com/decred/dcrd/dcrec/secp256k1

--- common/crypto/signsecp256k1.go
+++ common/crypto/signsecp256k1.go
@@ -1,38 +1,27 @@
 package crypto
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
+
+	// "madledger/common/crypto/secp256k1"
+
 	"encoding/hex"
 	"errors"
-	"fmt"
-	"math/big"
-
 	"madledger/common"
-	"madledger/common/crypto/secp256k1"
 	"madledger/common/crypto/sha3"
-	"madledger/common/math"
+
+	"github.com/decred/dcrd/dcrec/secp256k1"
 )
 
 // The secp256k1 implementation of sign.go
 
-var (
-	secp256k1N, _  = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
-	secp256k1halfN = new(big.Int).Div(secp256k1N, big.NewInt(2))
-)
-
 // SECP256K1PrivateKey defines the secp256k1 private key in ecdsa
-type SECP256K1PrivateKey ecdsa.PrivateKey
+type SECP256K1PrivateKey secp256k1.PrivateKey
 
 // SECP256K1PublicKey defines the secp256k1 public key in ecdsa
-type SECP256K1PublicKey ecdsa.PublicKey
-
-// SECP256K1SignatureLength defines the length of SECP256K1Signature
-const SECP256K1SignatureLength = 64
+type SECP256K1PublicKey secp256k1.PublicKey
 
 // SECP256K1Signature defines the secp256k1 signature
-type SECP256K1Signature [SECP256K1SignatureLength]byte
+type SECP256K1Signature secp256k1.Signature
 
 // HexToSECP256K1PrivateKey convert a hex string to PrivateKey
 func HexToSECP256K1PrivateKey(h string) (PrivateKey, error) {
@@ -45,31 +34,40 @@ func HexToSECP256K1PrivateKey(h string) (PrivateKey, error) {
 
 // GenerateSECP256K1PrivateKey return a new secp256k1 private key
 func GenerateSECP256K1PrivateKey() (PrivateKey, error) {
-	ecdsaPrivateKey, err := ecdsa.GenerateKey(S256(), rand.Reader)
+	privKey, err := secp256k1.GeneratePrivateKey()
 	if err != nil {
 		return nil, err
 	}
-	return (SECP256K1PrivateKey)(*ecdsaPrivateKey), nil
+	return (SECP256K1PrivateKey)(*privKey), nil
 }
 
 // PubKey returns the PublicKey corresponding to this private key.
 func (p SECP256K1PrivateKey) PubKey() PublicKey {
-	pk := (SECP256K1PublicKey)((ecdsa.PrivateKey)(p).PublicKey)
-	return (PublicKey)(pk)
+	var privKey = (secp256k1.PrivateKey)(p)
+	var pubKey = privKey.PubKey()
+	return (SECP256K1PublicKey)(*pubKey)
 }
 
 // Bytes is the implementation of interface
 func (p SECP256K1PrivateKey) Bytes() ([]byte, error) {
-	return math.PaddedBigBytes(p.D, p.Params().BitSize/8), nil
+	var privKey = (secp256k1.PrivateKey)(p)
+	return privKey.Serialize(), nil
+}
+
+// Sign sign the data using the privateKey
+func (p SECP256K1PrivateKey) Sign(hash []byte) (Signature, error) {
+	var privKey = (*secp256k1.PrivateKey)(&p)
+	sig, err := privKey.Sign(hash)
+	if err != nil {
+		return nil, err
+	}
+	return (SECP256K1Signature)(*sig), nil
 }
 
 // Bytes returns the bytes of Public key
 func (p SECP256K1PublicKey) Bytes() ([]byte, error) {
-	pk := (*(ecdsa.PublicKey))(&p)
-	if pk == nil || pk.X == nil || pk.Y == nil {
-		return nil, errors.New("The public key is not formatted")
-	}
-	return elliptic.Marshal(S256(), pk.X, pk.Y), nil
+	var pubKey = (secp256k1.PublicKey)(p)
+	return pubKey.SerializeUncompressed(), nil
 }
 
 // Address is the implementation of interface
@@ -83,97 +81,79 @@ func (p SECP256K1PublicKey) Address() (common.Address, error) {
 	return common.AddressFromBytes(addrBytes)
 }
 
-// Sign sign the data using the privateKey
-func (p SECP256K1PrivateKey) Sign(hash []byte) (Signature, error) {
-	var prv = (*ecdsa.PrivateKey)(&p)
-	if len(hash) != 32 {
-		return nil, fmt.Errorf("hash is required to be exactly 32 bytes (%d)", len(hash))
-	}
-	seckey := math.PaddedBigBytes(prv.D, prv.Params().BitSize/8)
-	defer zeroBytes(seckey)
-	sig, err := secp256k1.Sign(hash, seckey)
-	if err != nil {
-		return nil, err
-	}
-	return newSECP256K1Signature(sig)
-}
-
 // Verify is the implementation of interface
 func (s SECP256K1Signature) Verify(hash []byte, pubKey PublicKey) bool {
+	var sig = (secp256k1.Signature)(s)
 	switch pubKey.(type) {
 	case SECP256K1PublicKey:
+		var pk = (secp256k1.PublicKey)((pubKey).(SECP256K1PublicKey))
+		return sig.Verify(hash, &pk)
 	default:
 		return false
 	}
-	keyBytes, err := pubKey.Bytes()
-	if err != nil {
-		return false
-	}
-	return secp256k1.VerifySignature(keyBytes, hash, s[:])
 }
 
 // Bytes is the implementation of interface
 func (s SECP256K1Signature) Bytes() ([]byte, error) {
-	return s[:], nil
+	var sig = (secp256k1.Signature)(s)
+	return sig.Serialize(), nil
 }
 
 func newSECP256K1PublicKey(raw []byte) (PublicKey, error) {
-	x, y := elliptic.Unmarshal(S256(), raw)
-	if x == nil {
-		return nil, errors.New("Marshal failed")
-	}
-	return (SECP256K1PublicKey)(ecdsa.PublicKey{Curve: S256(), X: x, Y: y}), nil
-}
-
-func newSECP256K1Signature(raw []byte) (Signature, error) {
-	var sig SECP256K1Signature
-	copy(sig[:], raw)
-	return sig, nil
-}
-
-func toSECP256K1PrivateKey(bs []byte) (PrivateKey, error) {
-	priv, err := toECDSA(bs, false)
-	// return priv
+	pk, err := secp256k1.ParsePubKey(raw)
 	if err != nil {
 		return nil, err
 	}
+	return (SECP256K1PublicKey)(*pk), nil
+}
+
+func newSECP256K1Signature(raw []byte) (Signature, error) {
+	sig, err := secp256k1.ParseSignature(raw)
+	if err != nil {
+		return nil, err
+	}
+	return (SECP256K1Signature)(*sig), nil
+}
+
+func toSECP256K1PrivateKey(bs []byte) (PrivateKey, error) {
+	priv, _ := secp256k1.PrivKeyFromBytes(bs)
 	return (SECP256K1PrivateKey)(*priv), nil
 }
 
-// toECDSA creates a private key with the given D value. The strict parameter
-// controls whether the key's length should be enforced at the curve size or
-// it can also accept legacy encodings (0 prefixes).
-func toECDSA(d []byte, strict bool) (*ecdsa.PrivateKey, error) {
-	priv := new(ecdsa.PrivateKey)
-	priv.PublicKey.Curve = S256()
-	if strict && 8*len(d) != priv.Params().BitSize {
-		return nil, fmt.Errorf("invalid length, need %d bits", priv.Params().BitSize)
-	}
-	priv.D = new(big.Int).SetBytes(d)
+// // toECDSA creates a private key with the given D value. The strict parameter
+// // controls whether the key's length should be enforced at the curve size or
+// // it can also accept legacy encodings (0 prefixes).
+// func toECDSA(d []byte, strict bool) (*ecdsa.PrivateKey, error) {
+// 	priv := new(ecdsa.PrivateKey)
+// 	priv.PublicKey.Curve = S256()
+// 	if strict && 8*len(d) != priv.Params().BitSize {
+// 		return nil, fmt.Errorf("invalid length, need %d bits", priv.Params().BitSize)
+// 	}
+// 	priv.D = new(big.Int).SetBytes(d)
 
-	// The priv.D must < N
-	if priv.D.Cmp(secp256k1N) >= 0 {
-		return nil, fmt.Errorf("invalid private key, >=N")
-	}
-	// The priv.D must not be zero or negative.
-	if priv.D.Sign() <= 0 {
-		return nil, fmt.Errorf("invalid private key, zero or negative")
-	}
+// 	// The priv.D must < N
+// 	if priv.D.Cmp(secp256k1N) >= 0 {
+// 		return nil, fmt.Errorf("invalid private key, >=N")
+// 	}
+// 	// The priv.D must not be zero or negative.
+// 	if priv.D.Sign() <= 0 {
+// 		return nil, fmt.Errorf("invalid private key, zero or negative")
+// 	}
 
-	priv.PublicKey.X, priv.PublicKey.Y = priv.PublicKey.Curve.ScalarBaseMult(d)
-	if priv.PublicKey.X == nil {
-		return nil, errors.New("invalid private key")
-	}
-	return priv, nil
-}
+// 	priv.PublicKey.X, priv.PublicKey.Y = priv.PublicKey.Curve.ScalarBaseMult(d)
+// 	if priv.PublicKey.X == nil {
+// 		return nil, errors.New("invalid private key")
+// 	}
+// 	return priv, nil
+// }
 
-// S256 returns an instance of the secp256k1 curve.
-func S256() elliptic.Curve {
-	return secp256k1.S256()
-}
+// // S256 returns an instance of the secp256k1 curve.
+// func S256() elliptic.Curve {
+// 	return secp256k1.S256()
+// }
 
-func zeroBytes(bytes []byte) {
-	for i := range bytes {
-		bytes[i] = 0
-	}
-}
+// func zeroBytes(bytes []byte) {
+// 	for i := range bytes {
+// 		bytes[i] = 0
+// 	}
+// }
