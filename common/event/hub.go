8c57cd0ea01fe1f20582bc8ac6a2c118833c1ff7
liuyihua <i@liuyihua.com>
2018-11-12 21:24:46 +0800

    Add Watches function

--- common/event/hub.go
+++ common/event/hub.go
@@ -42,7 +42,7 @@ func (h *Hub) Done(id string, result *Result) {
 }
 
 // Watch will watch an event.
-// gc is still now be done yet.
+// todo: gc is still not done yet.
 func (h *Hub) Watch(id string, wc *WatchConfig) *Result {
 	h.lock.Lock()
 
@@ -74,4 +74,71 @@ func (h *Hub) Watch(id string, wc *WatchConfig) *Result {
 	defer h.lock.Unlock()
 
 	return result
+}
+
+// Watches will watch many events and return when all events are finished.
+func (h *Hub) Watches(ids []string) *Result {
+	h.lock.Lock()
+
+	// first check if there are event finished and contain error
+	for _, id := range ids {
+		// finished and err is not nil and err.Error() is not empty
+		if util.Contain(h.finish, id) && h.finish[id].Err != nil && h.finish[id].Err.Error() != "" {
+			defer h.lock.Unlock()
+			return h.finish[id]
+		}
+	}
+	// then ignore events that are finished and finish right
+	var unfinish []string
+	for _, id := range ids {
+		if !util.Contain(h.finish, id) {
+			unfinish = append(unfinish, id)
+			if !util.Contain(h.events, id) {
+				h.events[id] = make([]chan bool, 0)
+			}
+		}
+	}
+
+	// then set chans to watch these unfinished events
+	var chs = make(map[string]chan bool)
+	for _, id := range unfinish {
+		ch := make(chan bool, 1)
+		h.events[id] = append(h.events[id], ch)
+		chs[id] = ch
+	}
+	h.lock.Unlock()
+
+	// waitting events finish
+	var errs = make(chan error, len(unfinish))
+	for id := range chs {
+		go func(id string) {
+			<-chs[id]
+			h.lock.Lock()
+			result := h.finish[id]
+			h.lock.Unlock()
+			if result.Err != nil && result.Err.Error() != "" {
+				errs <- result.Err
+			} else {
+				errs <- nil
+			}
+		}(id)
+	}
+
+	var count = 0
+	var result = Result{
+		Err: nil,
+	}
+	for {
+		err := <-errs
+		if err != nil {
+			result.Err = err
+			break
+		}
+		count++
+		if count == len(unfinish) {
+			break
+		}
+	}
+
+	return &result
 }
--- common/event/hub_test.go
+++ common/event/hub_test.go
@@ -2,6 +2,7 @@ package event
 
 import (
 	"fmt"
+	"madledger/common/util"
 	"math/rand"
 	"sync"
 	"testing"
@@ -14,7 +15,7 @@ var (
 	eventSize = 2048
 )
 
-func TestHub(t *testing.T) {
+func TestWatch(t *testing.T) {
 	hub := NewHub()
 	events := make([]string, eventSize)
 	// initial events
@@ -57,6 +58,69 @@ func TestHub(t *testing.T) {
 	wg.Wait()
 }
 
+func TestWatches(t *testing.T) {
+	hub := NewHub()
+	events := make([]string, eventSize)
+	// initial events
+	for i := range events {
+		events[i] = randomStr()
+	}
+
+	var wg sync.WaitGroup
+	// register events
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < eventSize*10; i++ {
+			event := events[i%eventSize]
+			var es []string
+			es = append(es, events[i%eventSize])
+			// only watch succeed event if i%2=0
+			if i%2 == 0 {
+				es = append(es, events[(i+2)%eventSize])
+			} else {
+				es = append(es, events[(i+1)%eventSize])
+			}
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				randomSleep()
+				result := hub.Watch(event, nil)
+				if i%2 == 0 {
+					require.NoError(t, result.Err)
+				} else {
+					require.EqualError(t, result.Err, fmt.Sprintf("Error is %d", i%eventSize))
+				}
+			}(i)
+		}
+	}()
+
+	// finish events
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < eventSize; i++ {
+			event := events[i]
+			wg.Add(1)
+			go func(i int) {
+				wg.Done()
+				randomSleep()
+				if i%2 == 0 {
+					hub.Done(event, &Result{
+						Err: nil,
+					})
+				} else {
+					hub.Done(event, &Result{
+						Err: fmt.Errorf("Error is %d", i),
+					})
+				}
+			}(i)
+		}
+	}()
+
+	wg.Wait()
+}
+
 func randomStr() string {
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	b := make([]rune, 32)
@@ -65,4 +129,8 @@ func randomStr() string {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(b)
+}
+
+func randomSleep() {
+	time.Sleep(time.Duration(util.RandNum(500)) * time.Millisecond)
 }
