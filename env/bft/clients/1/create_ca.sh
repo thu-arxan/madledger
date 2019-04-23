52b88cc6c13e41fe1744bb87506e175465974009
sunriseLe <1379960362@qq.com>
2019-04-23 11:03:25 +0800

    update create_ca.sh env/bft/clients

--- env/bft/clients/1/create_ca.sh
+++ env/bft/clients/1/create_ca.sh
@@ -4,7 +4,7 @@ echo 'Test bft consensus by creating contract...'
 for ((i=1; i<=8; i++))
 do
   echo 'create contract '$i
-  client tx create -b /home/hadoop/solidity/MyTest.bin -n test1
+  client tx create -b ./MyTest.bin -n test1
   #client channel list
   #echo $i
 done
