6ffaa7cfcf979fd6049e70fe10cadddf0213b6dd
liuyihua <i@liuyihua.com>
2019-05-29 11:22:13 +0800

    update log app

--- consensus/raft/init.go
+++ consensus/raft/init.go
@@ -3,6 +3,6 @@ package raft
 import "github.com/sirupsen/logrus"
 
 var (
-	log     = logrus.WithFields(logrus.Fields{"app": "blockchain", "package": "raft"})
-	etcdLog = logrus.WithFields(logrus.Fields{"app": "blockchain", "package": "etcd/raft"})
+	log     = logrus.WithFields(logrus.Fields{"app": "consensus", "package": "raft"})
+	etcdLog = logrus.WithFields(logrus.Fields{"app": "consensus", "package": "etcd/raft"})
 )
