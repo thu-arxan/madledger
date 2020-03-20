50acf9c8c2efc23b6c18c259de944d2fd913080a
liuyihua <i@liuyihua.com>
2020-03-20 17:59:12 +0800

    remove useless openss/c code

--- common/crypto/openssl/c/build.sh
+++
@@ -1,6 +0,0 @@
-src="uid_sm2.c"
-if [ "$1" != "" ]; then
-    src="$1"
-fi
-# gcc -Wall  -o main -L/usr/local/openssl/lib -lcrypto -lssl -I/usr/local/openssl/include  util.c $src
-gcc -g -o main  $src -Wall -L/usr/local/openssl/lib -lcrypto -lssl -I/usr/local/openssl/include
--- common/crypto/openssl/c/command.md
+++
@@ -1,8 +0,0 @@
-# 常用命令
-
-## 签名验签
-
-```sh
-openssl dgst -sign priv.pem -sha256 -out sign.txt in.txt
-openssl dgst -verify pub.pem -sha256 -signature sign.txt in.txt
-```
