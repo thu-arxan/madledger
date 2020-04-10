e75cb2ce9c227a9d6ba33a6e77f7136af7d62b8d
Mao Hanyang <maohanyang789@163.com>
2020-04-10 14:30:22 +0800

    fix some bug to support backend

--- explorer/server/root.go
+++ explorer/server/root.go
@@ -8,10 +8,29 @@ import (
 var SessionPool = make(map[string] *Session)
 var defaultConfig = ""
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+
 func RunServer(host string, port int, config string) error {
 	defaultConfig = config
 	fmt.Printf("Server run on %s:%d\n", host, port)
 	r := gin.Default()
+	r.Use(CORSMiddleware())
+	r.Static("/static", "./static")
 	r.GET("/api/client/account/list", AccountList)
 	r.GET("/api/client/channel/list", ChannelList)
 	r.GET("/api/client/tx/history", TxHistory)
